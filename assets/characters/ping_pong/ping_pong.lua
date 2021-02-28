59419282c4826bd34144fb870a42b194cef611e5
Evan Lanham <44246276+Aquaticholic@users.noreply.github.com>
2021-02-27 19:55:22 -0700

    YOOOOOOOO

--- assets/characters/ping_pong/ping_pong.lua
+++ assets/characters/ping_pong/ping_pong.lua
@@ -2,24 +2,36 @@ function OnCreate()
   PlayAnimation("Idle")
 end
 
+function MoveCancel()
+  if Input.moveX ~= 0 or Input.moveY ~= 0 then
+    PlayAnimation("Run")
+  end
+end
 
 
 function Idle()
   -- Print(Input.moveX)
+  if (Input.button1) then
+    PlayAnimation("Forehand")
+  end
   if Input.moveX ~= 0 or Input.moveY ~= 0 then
     PlayAnimation("Run")
   end
 end
 
 function Idle_Start()
-  LookAtMouse()
+  LockRotation()
+end
+
+function Idle_End()
+  UnlockRotation()
 end
 
 
 
 function Run()
   if (Input.button1) then
-    Print("button1")
+    PlayAnimation("Forehand")
   end
   if Input.moveX == 0 and Input.moveY == 0 then
     PlayAnimation("Idle")
@@ -28,20 +40,101 @@ end
 
 function Run_Start()
   LookAtMotion()
-end
-
-function Run_End()
-end
-
-function Run_0()
+  UnlockMotion()
+  UnlockRotation()
 end
 
 
 
-
-function Clap()
+function Forehand()
 end
-
-function Clap_End()
+function Forehand_Start()
+  LockMotion()
+  LookAtMouse()
+  LockRotation()
+end
+function Forehand_End()
+end
+function Forehand_Last()
+  UnlockMotion()
   PlayAnimation("Idle")
 end
+function Forehand_17()
+  if (Input.button2) then
+    PlayAnimation("Backhand", true)
+  elseif (Input.button1) then
+    PlayAnimation("Push", true)
+  end
+end
+function Forehand_10()
+  PlaySound(Sounds.whoosh1)
+  AddTrauma(0.3)
+end
+
+
+
+function Backhand()
+  if (Data.frame >= 40) then 
+    MoveCancel()
+  end
+end
+function Backhand_Start()
+end
+function Backhand_End()
+  UnlockMotion()
+end
+function Backhand_Last()
+  PlayAnimation("Idle")
+end
+function Backhand_18()
+  PlaySound(Sounds.whoosh2)
+  AddTrauma(0.8)
+end
+
+
+
+function Push()
+  if (Data.frame >= 26) then 
+    MoveCancel()
+  end
+end
+function Push_Start()
+end
+function Push_End()
+  UnlockMotion()
+end
+function Push_Last()
+  PlayAnimation("Idle")
+end
+function Push_7()
+  PlaySound(Sounds.whoosh1)
+  AddTrauma(0.3)
+end
+
+
+-- function Clap()
+--   if Data.frame >= 20 then
+--     MoveCancel()
+--   end
+-- end
+
+-- function Clap_0()
+--   LookAtMouse()
+--   LockRotation()
+--   -- LockMotion(2)
+--   SetMovementSpeed(0.3)
+-- end
+
+-- function Clap_17()
+--   UnlockMotion()
+-- end
+
+-- function Clap_End()
+--   UnlockRotation()
+--   -- UnlockMotion()
+--   SetMovementSpeed(1.0)
+-- end
+
+-- function Clap_Last()
+--   PlayAnimation("Idle")
+-- end
